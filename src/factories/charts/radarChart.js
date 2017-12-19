67054ca6d43d51bb870c30bd10e04812ff970784
Dênnis Dantas <ddspog@gmail.com>
2017-12-19 17:10:37 -0200

    fix(factories): Fix calls to old version of d3 :bug:
    I've changed some calls that were gibing some errors, since some methods
    were deprecated from d3 v3 to v4.

--- src/factories/charts/radarChart.js
+++ src/factories/charts/radarChart.js
@@ -24,7 +24,7 @@ function radarChart(d3, id, data, options) {
         opacityCircles: 0.1, //The opacity of the circles of each blob
         strokeWidth: 2, //The width of the stroke around each blob
         roundStrokes: false, //If true the area and stroke will follow a round path (cardinal-closed)
-        color: d3.scale.category10() //Color function
+        color: d3.scaleOrdinal(d3.schemeCategory10) //Color function
     };
 
     //Put all of the options into a variable called cfg
@@ -52,7 +52,7 @@ function radarChart(d3, id, data, options) {
         angleSlice = Math.PI * 2 / total; //The width in radians of each "slice"
 
     //Scale for the radius
-    var rScale = d3.scale.linear()
+    var rScale = d3.scaleLinear()
         .range([0, radius])
         .domain([0, maxValue]);
 
@@ -166,8 +166,8 @@ function radarChart(d3, id, data, options) {
     /////////////////////////////////////////////////////////
 
     //The radial line function
-    var radarLine = d3.svg.line.radial()
-        .interpolate("linear-closed")
+    var radarLine = d3.radialLine()
+        .curve(d3.curveLinearClosed)
         .radius(function (d) {
             return rScale(d.value);
         })
